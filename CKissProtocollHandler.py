89b7a482d3430a8a453a25f431e6f3626d0189bb
trlink <57213091+trlink@users.noreply.github.com>
2021-12-28 14:55:08 +0100

    Added files

--- CKissProtocollHandler.py
+++ CKissProtocollHandler.py
@@ -1,23 +1,8 @@
 import time
 import logging
+import zlib
 
 
-FEND = 0xC0
-FESC = 0xDB
-TFEND= 0xDC
-TFESC= 0xDD
-            
-KISSCMD_DATA = 0x0
-KISSCMD_P = 0x02
-KISSCMD_SlotTime = 0x03
-KISSCMD_NoCmd = 0x80
-
-KISSSTATE_VOID = 0x0
-KISSSTATE_GETCMD = 0x1
-KISSSTATE_GETDATA = 0x2
-KISSSTATE_GETP = 0x3
-KISSSTATE_GETSLOTTIME = 0x4
-KISSSTATE_ESC = 0x5
   
 class CKissProtocollHandler:
     
@@ -28,10 +13,15 @@ class CKissProtocollHandler:
         self.m_nMaxDataLen = nMaxDataLen
         self.m_logging = logging.getLogger("CKissProtocollHandler")
         self.m_logging.debug("Init CKissProtocollHandler, Buff size: " + str(nMaxDataLen))
-        self.m_nKissState = KISSSTATE_VOID
-        self.m_nKissCmd = KISSCMD_NoCmd
         self.m_bData = bytearray(b'')
         self.m_aPackets = []
+        
+        
+    def haveData(self):
+        if len(self.m_aPackets) > 0:
+            return True
+        else:
+            return False
         
 
     def addData(self, bData):
@@ -62,24 +52,7 @@ class CKissProtocollHandler:
         self.m_logging.debug("CKissProtocollHandler::Encode: " + str(len(bData)) + " bytes of Data")
         
         if len(bData) > 0:
-            
-            #bRes.append(FEND)
-            #bRes.append(KISSCMD_DATA)
-            
-            for b in bData:
-                
-                #if b == FEND:
-                #    bRes.append(FESC)
-                #    bRes.append(TFEND)
-                
-                #elif b == FESC:
-                #    bRes.append(FESC)
-                #    bRes.append(TFESC)
-                
-                #else:
-                bRes.append(b)
-                                    
-            #bRes.append(FEND)
+            bRes = bData
 
         self.m_logging.debug("CKissProtocollHandler::Encode Result: " + str(len(bRes)) + " bytes of Data")
 
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import time
 import logging
 import os, pty, serial
+import sys
 from lora02_modem import CLoRa02_Modem
 from CKissProtocollHandler import CKissProtocollHandler
 from SerialEmulator import SerialEmulator
+import configparser
+
 
 
 #https://github.com/sh123/lora_arduino_kiss_modem/blob/master/lora_arduino_kiss_modem.ino
@@ -11,27 +14,57 @@ from SerialEmulator import SerialEmulator
 
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
         
+
+g_nErrCount = 0;
 g_log = logging.getLogger("main")
 g_nMaxProtoLen = 128
 g_modem = None
+g_strDev = ""
+g_nRST = 0;
+g_nIRQ = 0
+g_nCS = 0
+g_lFreq = 0
+g_nSF = 0
+g_nBandwidth = 0
+g_bPreamble = 0
+g_nSyncWord = 0
+g_nCodingRate = 0
 
 
 def initHardware():
     #variables
     ##########
     global g_modem
+    global g_nErrCount
+    global g_lFreq
+    global g_nSF
+    global g_nBandwidth
+    global g_bPreamble
+    global g_nSyncWord
+    global g_nCodingRate
+    global g_strDev
+    global g_nRST
+    global g_nIRQ
+    global g_nCS
+
     
     #modem = CLoRa02_Modem("/dev/spidev0.0", 24, 22, 16)
-    g_modem = CLoRa02_Modem("/dev/spidev0.0", 24, 22, 16)
-    g_modem.SetModemOptions({"Frequency": 433000000, "SF": 10, "BW": 125000, "Preamble": 200, "SW": 99, "CR": 4})
+    g_modem = CLoRa02_Modem(g_strDev, g_nCS, g_nRST, g_nIRQ)
+    g_modem.SetModemOptions({"Frequency": g_lFreq, "SF": g_nSF, "BW": g_nBandwidth, "Preamble": g_bPreamble, "SW": g_nSyncWord, "CR": g_nCodingRate})
     
     if g_modem.InitModem() == True:
         return True
     else:
         g_log.error("Failed to initialize LoRa-Module")
+        g_nErrCount += 1;
+        
+        if g_nErrCount > 5:
+            g_log.error("Can't init modem, give up...")
+            
+            return False;
         
         #init with wrong CS, on next call, it could work...
-        g_modem = CLoRa02_Modem("/dev/spidev0.0", 25, 22, 16)
+        g_modem = CLoRa02_Modem(g_strDev, g_nCS + 1, g_nRST, g_nIRQ)
         g_modem.InitModem()
         
         time.sleep(2)
@@ -46,11 +79,39 @@ if __name__ == "__main__":
     kissSerial = CKissProtocollHandler(g_nMaxProtoLen)
 
 
-    g_log.info("Start LoRa02 to KISS Bridge")
+    g_log.info("Start LoRa02 to Serial Bridge")
     
+    config = configparser.ConfigParser()
+    config.read('/etc/seriallora.cfg')
+
+    try:
+        g_strDev = config["modem"]["device"]
+        g_nRST = int(config["modem"]["RST"])
+        g_nIRQ = int(config["modem"]["IRQ"])
+        g_nCS = int(config["modem"]["CS"])
+        g_lFreq = int(config["lora"]["freq"])
+        g_nSF = int(config["lora"]["SF"])
+        g_nBandwidth = int(config["lora"]["BW"])
+        g_bPreamble = int(config["lora"]["Preamble"])
+        g_nSyncWord = int(config["lora"]["SyncWord"])
+        g_nCodingRate = int(config["lora"]["CodingRate"])      
+
+    except:
+        g_log.error("could not read config, check config!")
+
+        sys.exit(-1)
+
+        
     if initHardware() == True:
-        emulator = SerialEmulator('./ttydevice','/dev/lora0', 9600) 
-    
+        
+        try:
+            emulator = SerialEmulator('./ttydevice','/dev/lora0', 9600) 
+        except:
+            g_log.error("could not Create virtual ports - start with root rights!")
+
+            sys.exit(-1)
+
+        
         g_log.debug("Created virtual ports")
 
         bHavePacket = False
@@ -60,18 +121,24 @@ if __name__ == "__main__":
             bData = b'' 
             bData = emulator.read()
             
-            if len(bData) > 0:
+            if (len(bData) > 0):
                 
-                g_log.debug("Serial: received: " + str(len(bData)))
+                if (len(bData) <= 250):
                 
-                if kissSerial.addData(bData) == True:
+                    g_log.debug("Serial: received: " + str(len(bData)))
+                    kissSerial.addData(bData)
+                
+                else:
                     
-                    bHavePacket = True
+                    g_log.warning("Serial: packet over max trx size, received: " + str(len(bData)))
+            
+            
+            bHavePacket = kissSerial.haveData()      
                     
             if bHavePacket == True:
                 
                 if g_modem.isReceiving() == False:
-                    bHavePacket = False
+                    
                     bData = kissSerial.getPacket()
             
                     if len(bData) > 0:
@@ -103,4 +170,5 @@ if __name__ == "__main__":
         
     else:
         g_log.eror("Unable to init hardware")
-                
+        
+        sys.exit(-1)
